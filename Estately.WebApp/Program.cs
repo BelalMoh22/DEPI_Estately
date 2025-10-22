0b34a59324bf5ad4b0f93ca17729518f03524a13
Belal Mohamed <ibelalmohammed@gmail.com>
2025-10-22 20:41:39 +0300

    Update Project

--- Estately.WebApp/Program.cs
+++ Estately.WebApp/Program.cs
@@ -38,6 +38,8 @@ namespace Estately.WebApp
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            
+
 
             app.UseHttpsRedirection();
             app.UseRouting();
