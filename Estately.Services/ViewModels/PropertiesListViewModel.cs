6a23e395777fbeaf3df4b67846bfb37cee2904fc
Yussif Abdelmaqsoud <yussifabdelmaqsoud2003@gmail.com>
2025-11-16 20:01:35 +0200

    properties listing

---
+++ Estately.Services/ViewModels/PropertiesListViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estately.Services.ViewModels
+{
+    public class PropertiesListViewModel
+    {
+        public int PropertyID { get; set; }
+        public string Address { get; set; } = "";
+        public string CityName { get; set; } = "";
+        public string ZoneName { get; set; } = "";
+        public decimal Price { get; set; }
+        public int Beds { get; set; }
+        public int Baths { get; set; }
+
+        public string FirstImage { get; set; } = "default.jpg";
+    }
+}
--- Estately.WebApp/Controllers/AppController.cs
+++ Estately.WebApp/Controllers/AppController.cs
@@ -1,7 +1,15 @@
-﻿namespace Estately.WebApp.Controllers
+﻿using Estately.Services.ViewModels;
+
+namespace Estately.WebApp.Controllers
 {
     public class AppController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public AppController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
         public IActionResult Index()
         {
             return View();
@@ -22,9 +30,58 @@
         {
             return View();
         }
-        public IActionResult Properties() 
+        [HttpGet]
+        public async Task<IActionResult> Properties(int page = 1, int pageSize = 9)
         {
-            return View();
+            var all = await _unitOfWork.PropertyRepository.ReadAllIncluding(
+                "Zone",
+                "Zone.City"
+            );
+
+            int totalCount = all.Count();
+
+            var paged = all
+                .OrderBy(p => p.PropertyID)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            var model = new PagedPropertyViewModel
+            {
+                Page = page,
+                PageSize = pageSize,
+                TotalCount = totalCount
+            };
+
+            string folder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/properties");
+
+            foreach (var p in paged)
+            {
+                // Load only FIRST image
+                string firstImagePattern = $"prop-{p.PropertyID}-1.*";
+
+                var firstImage = Directory
+                    .EnumerateFiles(folder, firstImagePattern)
+                    .Select(Path.GetFileName)
+                    .FirstOrDefault();
+
+                if (firstImage == null)
+                    firstImage = "default.jpg";
+
+                model.Properties.Add(new PropertiesListViewModel
+                {
+                    PropertyID = p.PropertyID,
+                    Address = p.Address,
+                    CityName = p.Zone?.City?.CityName ?? "",
+                    ZoneName = p.Zone?.ZoneName ?? "",
+                    Price = p.Price,
+                    Beds = p.BedsNo,
+                    Baths = p.BathsNo,
+                    FirstImage = firstImage
+                });
+            }
+
+            return View(model);
         }
         public IActionResult PropertySingle() 
         {
